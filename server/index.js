f017477bc5787f00bfc72fa3f557d92b46d5030e
maherzaidoune <maherzaidoune@gmail.com>
2021-05-30 20:43:00 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -22,19 +22,19 @@ var chatId = 1;
 
 websocket.on('connection', (socket) => {
     clients[socket.id] = socket;
-    socket.on('userJoined', (user, room) => onUserJoined(user, room, socket));
+    // socket.on('userJoined', (user, room) => onUserJoined(user, room, socket));
     socket.on('message', (message) => onMessageReceived(message, socket));
 });
 
 function onUserJoined(user, room, socket) {
-  try {
-  var user = db.collection('users').insert({user: user, room: room}, (err, user) => {
-      socket.join(room);
-  });
+  // try {
+  // var user = db.collection('users').insert({user: user, room: room}, (err, user) => {
+  //     socket.join(room);
+  // });
     
-  } catch(err) {
-    console.err(err);
-  }
+  // } catch(err) {
+  //   console.err(err);
+  // }
 }
 
 function onMessageReceived(message, senderSocket) {
@@ -42,15 +42,15 @@ function onMessageReceived(message, senderSocket) {
   _sendAndSaveMessage(message, senderSocket);
 }
 
-function _sendExistingMessages(socket) {
-  var messages = db.collection('messages')
-    .find({ chatId })
-    .sort({ createdAt: 1 })
-    .toArray((err, messages) => {
-      if (!messages.length) return;
-      socket.emit('message', messages.reverse());
-  });
-}
+// function _sendExistingMessages(socket) {
+//   var messages = db.collection('messages')
+//     .find({ chatId })
+//     .sort({ createdAt: 1 })
+//     .toArray((err, messages) => {
+//       if (!messages.length) return;
+//       socket.emit('message', messages.reverse());
+//   });
+// }
 function _sendAndSaveMessage(message, socket, fromServer) {
 
 
@@ -64,27 +64,27 @@ function _sendAndSaveMessage(message, socket, fromServer) {
   // };
 
   if (message.text === '') return
-  db.collection('messages').insert(message, (err, message) => {
+  // db.collection('messages').insert(message, (err, message) => {
     //var emitter = fromServer ? websocket : socket.broadcast;
 
-    socket.broadcast.to(message.room).emit('message', message);
-  });
+    socket.broadcast.to('workshop').emit('message', message);
+  // });
 }
-var stdin = process.openStdin();
-stdin.addListener('data', function(d) {
-  _sendAndSaveMessage({
-    text: d.toString().trim(),
-    createdAt: new Date(),
-    user: { _id: 'robot' }
-  }, null , true );
-});
+// var stdin = process.openStdin();
+// stdin.addListener('data', function(d) {
+//   _sendAndSaveMessage({
+//     text: d.toString().trim(),
+//     createdAt: new Date(),
+//     user: { _id: 'robot' }
+//   }, null , true );
+// });
 
-app.get('/rooms', function(req, res) {
-  db.collection('chatroom').find(function (err, chatrooms) {
-    res.send({chatrooms});  
-  })
+// app.get('/rooms', function(req, res) {
+//   db.collection('chatroom').find(function (err, chatrooms) {
+//     res.send({chatrooms});  
+//   })
   
-});
+// });
 
 // const schema = Joi.object().keys({
 //   name: Joi.string().min(3).max(30).required(),
@@ -92,17 +92,17 @@ app.get('/rooms', function(req, res) {
 //   avatarUrl: Joi.string().min(10).max(300)
 // });
 
-app.post('/rooms', function (req, res) {
-  console.log(req.body);
-  //const result = Joi.validate(req.body, schema);
-  //console.log(result);
-  //if(result.error === null){
-    db.collection('chatroom').insert(req.body, (err, chatroom) => {
-      console.log(chatroom);
-      res.status(200).send({chatroom});
-    });
-  // }else{
-  //   res.status(422).send({error: 'format invalid'});
-  // }
+// app.post('/rooms', function (req, res) {
+//   console.log(req.body);
+//   //const result = Joi.validate(req.body, schema);
+//   //console.log(result);
+//   //if(result.error === null){
+//     db.collection('chatroom').insert(req.body, (err, chatroom) => {
+//       console.log(chatroom);
+//       res.status(200).send({chatroom});
+//     });
+//   // }else{
+//   //   res.status(422).send({error: 'format invalid'});
+//   // }
   
-});
+// });
